5e81ca00e04fdc5376718d9468bc0ca1b2cf9810
WASIM ANSARI <147571204+WasimAman@users.noreply.github.com>
2025-05-07 12:39:58 +0530

    Update CalculatorTest.java

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -1,14 +1,17 @@
 package in.javahome.myweb.controller;
 
-import junit.framework.Assert;
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CalculatorTest extends TestCase {
-	Calculator cal = new Calculator();
-	public void testAdd(){
-		Assert.assertEquals(cal.add(10, 20), 30);
-	}
-	public void testMultiply(){
-		Assert.assertEquals(cal.multiply(10, 20), 200);
-	}
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(30, 10 + 20);
+    }
+
+    @Test
+    public void testSub() {
+        assertEquals(10, 20 - 10);
+    }
 }
